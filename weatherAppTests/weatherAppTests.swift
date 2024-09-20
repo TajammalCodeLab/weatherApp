4d1370f916eab11a657ef702ccc0284c011297e2
SID <sid@Tajammal-Hussains-mac.local>
2024-09-20 23:32:51 +0500

    initail comments,uplaoding to github

---
+++ weatherAppTests/weatherAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppTests.swift
+//  weatherAppTests
+//
+//  Created by SID on 20/09/2024.
+//
+
+import Testing
+@testable import weatherApp
+
+struct weatherAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
